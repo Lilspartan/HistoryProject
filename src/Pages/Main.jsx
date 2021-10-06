0254aec587c1262eee0a18039b24c5c31c65e585
Gabe Krahulik <44793856+Lilspartan@users.noreply.github.com>
2021-10-06 18:15:16 +0000

    some cooler animations

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -11,9 +11,29 @@ const Main = () => {
         }
     }
 
+		const Button = ({ name, type, text }) => {
+			return (
+				<div className = "t">
+						<button class={`uk-button uk-button-${type} uk-button-large t`} type = "button">{ name }</button>
+						<div uk-drop="pos: top; offset: 50">
+								<div class="uk-card uk-card-body uk-card-default">
+										{text}
+								</div>
+						</div>
+				</div>
+			)
+		}
+
     return (
         <div className = "uk-background-secondary" style = {{ height: 'auto', minHeight: '100vh' }}>
-            <h1 style = {{ color: 'white', textAlign: 'center' }}>Different Views About...</h1>
+					<div style = {{ textAlign: 'center', paddingTop: '30px' }} uk-scrollspy="target: > h1; cls: uk-animation-fade; delay: 500">
+							<h1 style = {{ color: 'white', textAlign: 'center' }}>Different </h1>
+							<h1 style = {{ color: 'white', textAlign: 'center' }}> Views </h1>
+							<h1 style = {{ color: 'white', textAlign: 'center' }}> About</h1>
+							<h1 style = {{ color: 'white', textAlign: 'center' }}>.</h1>
+							<h1 style = {{ color: 'white', textAlign: 'center' }}>.</h1>
+							<h1 style = {{ color: 'white', textAlign: 'center' }}>.</h1>
+						</div>
             <div style = {{ width: '30vw', margin: 'auto', padding: '20px'}}>
                 <select class="uk-select" onChange = {onChange}>
                     {data.map(event => (
@@ -23,45 +43,14 @@ const Main = () => {
             </div>
 
             <div>
-                <div id = "btnarea" uk-scrollspy="target: > .t; cls: uk-animation-slide-bottom-small; delay: 200">
-                    <div className = "t">
-                        <button class="uk-button uk-button-danger uk-button-large t" type = "button">Reactionary</button>
-                        <div uk-drop="pos: top">
-                            <div class="uk-card uk-card-body uk-card-default">
-                                { chosen.views.reactionary }
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className = "t">
-                        <button class="uk-button uk-button-danger uk-button-large t" type = "button">Conservative</button>
-                        <div uk-drop="pos: top">
-                            <div class="uk-card uk-card-body uk-card-default">
-                                { chosen.views.conservative }
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className = "t">
-                        <button class="uk-button uk-button-primary uk-button-large t" type = "button">Liberal</button>
-                        <div uk-drop="pos: top">
-                            <div class="uk-card uk-card-body uk-card-default">
-                                { chosen.views.liberal }
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className = "t">
-                        <button class="uk-button uk-button-primary uk-button-large t" type = "button">Radical</button>
-                        <div uk-drop="pos: top">
-                            <div class="uk-card uk-card-body uk-card-default">
-                                { chosen.views.radical }
-                            </div>
-                        </div>
-                    </div>
+                <div id = "btnarea" uk-scrollspy="target: > .t; cls: uk-animation-scale-up; delay: 250">
+                    <Button name = "Reactionary" type = "danger" text = {chosen.views.reactionary} />
+                    <Button name = "Conservative" type = "danger" text = {chosen.views.conservative} />
+                    <Button name = "Liberal" type = "primary" text = {chosen.views.liberal} />
+                    <Button name = "Radical" type = "primary" text = {chosen.views.radical} />
                 </div>
             </div>
-            <footer style = {{ position: 'absolute', bottom: '0', display: 'block', width: '100vw', textAlign: 'center' }}>Made by Gabe Krahulik</footer>
+            <footer className = "uk-animation-slide-bottom">Made by Gabe Krahulik</footer>
         </div>
     )
 }
